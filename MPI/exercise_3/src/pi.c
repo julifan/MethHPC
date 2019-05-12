f2f57c158725239c403e747d7011cb772cbac41b
Larissa <larschmid@web.de>
2019-05-12 20:38:20 +0200

    solved exercise 3

--- MPI/exercise_3/src/pi.c
+++ MPI/exercise_3/src/pi.c
@@ -23,4 +23,49 @@ void cleanup_pi()
 
 void compute_pi(int flip, int *local_count, double *answer)
 {
+
+    int num_ranks;
+    int world_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int count = 0;
+    double x, y, z;
+
+    seed = seed * world_rank;
+    srand(seed); // Important: Multiply SEED by "rank" when you introduce MPI!
+
+    // Calculate PI following a Monte Carlo method
+    int iter;
+    for (iter = 0; iter * num_ranks + world_rank < flip; iter++)
+    {
+        // Generate random (X,Y) points
+        x = (double)random() / (double)RAND_MAX;
+        y = (double)random() / (double)RAND_MAX;
+        z = sqrt((x*x) + (y*y));
+
+        // Check if point is in unit circle
+        if (z <= 1.0)
+        {
+            *local_count += 1;
+        }
+    }
+
+    int count_temp[num_ranks];
+
+    MPI_Gather(local_count, 1, MPI_INT, &count_temp, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        count_temp[0] = *local_count;
+        int i;
+        for (i = 0; i < num_ranks; i++) {
+            count += count_temp[i];
+        }
+
+        // Estimate Pi and display the result
+        *answer = ((double)count / (double)flip) * 4.0;
+    }
+
+
 }
