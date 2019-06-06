a5e3b8d6ce323d9a948fbce4f5f1507e73c14723
Alexander Gaul <alexander.gaul@tum.de>
2019-06-06 14:55:10 +0200

    minor fix

--- MapReduce/src/usecase.cpp
+++ MapReduce/src/usecase.cpp
@@ -27,8 +27,8 @@ Pair map(char* block, int* moved, const int totalLength)
 			}
 		} else if (toReturn.length() == MAX_LENGTH) {
 			Pair tup(toReturn, 1);
-			block = block + 1; 
-			*moved += 1;
+			// block = block + 1; 
+			//*moved += 1;
 			return tup;
 		} else {
 			toReturn.append(block, 1);
